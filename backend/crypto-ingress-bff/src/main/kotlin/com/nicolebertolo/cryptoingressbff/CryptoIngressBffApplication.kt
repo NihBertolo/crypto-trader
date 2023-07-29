65304dfa06f8e25605c95dc51c7ce40b11a3cf6f
nihbertolo <nickbertolo@gmail.com>
2023-07-29 02:45:37 -0300

    first-commit

---
+++ backend/crypto-ingress-bff/src/main/kotlin/com/nicolebertolo/cryptoingressbff/CryptoIngressBffApplication.kt
@@ -0,0 +1,11 @@
+package com.nicolebertolo.cryptoingressbff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoIngressBffApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoIngressBffApplication>(*args)
+}
---
+++ backend/crypto-ingress-bff/src/test/kotlin/com/nicolebertolo/cryptoingressbff/CryptoIngressBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicolebertolo.cryptoingressbff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoIngressBffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
